175dacd6f6ea2fa5eec5d7c9e50f261a4db99779
root <root@pawel-Satellite-A500>
2016-01-17 12:44:10 +0100

    pierwszy test

--- src/test/java/pl/macierze/test/AllTests.java
+++ src/test/java/pl/macierze/test/AllTests.java
@@ -5,23 +5,33 @@ package pl.macierze.test;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import pl.macierze.utils.Helpers;
 
 /**
  *
  * @author root
  */
-public class TestAdd {
-    
-    public TestAdd() {
-    }
-    
+public class AllTests {
 
-    // public void hello() {}
+    public AllTests() {
+    }
+
+    @Test
+    public void testParseInputCorrect() {
+        System.out.println("pl.macierze.test.TestAdd.testParseInput()");
+
+        int[] expecteds = {1, 2, 3, 4};
+
+        int[] actuals = Helpers.parseInput("1234");
+
+        assertArrayEquals(expecteds, actuals);
+    }
+
+
 }
